6a49ca2c00408f083ad833d48a390b98bfb3797f
Alex Wild <alex.ladell.wild@gmail.com>
2022-10-15 10:07:49 -0600

    Updates

---
+++ precache-manifest.a33e6dd3d1c9a911a42b8b8cd37c5aba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf521cf7bf8395b1b33d",
+    "url": "/zootah-giftstore/static/css/main.064d5ca3.chunk.css"
+  },
+  {
+    "revision": "bf521cf7bf8395b1b33d",
+    "url": "/zootah-giftstore/static/js/main.bf521cf7.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/zootah-giftstore/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "16ee3d5197cefc26336f",
+    "url": "/zootah-giftstore/static/js/2.16ee3d51.chunk.js"
+  },
+  {
+    "revision": "7f7ff7bd16f5143af452",
+    "url": "/zootah-giftstore/static/js/runtime~main.7f7ff7bd.js"
+  },
+  {
+    "revision": "867a6eb92f5f79d2cfbd8529ce1bb6f2",
+    "url": "/zootah-giftstore/static/media/zootahCup.867a6eb9.jpeg"
+  },
+  {
+    "revision": "78e5da5593419d3e8029e3e67b77cb0c",
+    "url": "/zootah-giftstore/index.html"
+  }
+];
